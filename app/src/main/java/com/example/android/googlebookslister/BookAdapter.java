cbde999b45f04ac8f7848f4d5eb0f5247d27d19f
rreay724 <rreay724@gmail.com>
2018-12-07 18:36:17 -0500

    Updated main screen. Previously had the "Google books" logo, but that is not complient to upload to google play store. Added more of a generic graphic with title for now.

--- app/src/main/java/com/example/android/googlebookslister/BookAdapter.java
+++ app/src/main/java/com/example/android/googlebookslister/BookAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.googlebookslister;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -57,9 +59,8 @@ public class BookAdapter extends ArrayAdapter<Book> {
     // Helper method to check for missing summary
     private String checkForSummary(String description) {
         if (description == null || description.isEmpty()) {
-            return description = "Description is not available.";
+            description = "Description is not available.";
         }
         return description;
     }
-
 }
